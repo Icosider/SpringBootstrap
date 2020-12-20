07a921078b0a59630c40e79745bc7439b39f29a7
CKATEPTb <taksis@ex.ua>
2020-12-20 19:25:15 +0200

    At the moment, we don't need the whole SpringBoot, just the SpringContext is enough.

--- src/main/java/ru/ckateptb/itlfts/SpringMod.java
+++ src/main/java/ru/ckateptb/itlfts/SpringMod.java
@@ -1,8 +1,8 @@
 package ru.ckateptb.itlfts;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.switchyard.common.type.CompoundClassLoader;
 
@@ -20,20 +20,31 @@ public abstract class SpringMod {
 
     public SpringMod() {
         mods.add(this);
-        classLoaders.add(this.getClass().getClassLoader());
+        Class<? extends SpringMod> clazz = this.getClass();
+        classLoaders.add(clazz.getClassLoader());
+        log.info("Found a mod that requires Spring: {}", clazz);
     }
 
     void refresh() {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
+        log.info("Spring starts to initialize all found class loaders ({})", classLoaders.size());
         try {
             classLoaders.add(Thread.currentThread().getContextClassLoader());
             ClassLoader classLoader = new CompoundClassLoader(classLoaders.toArray(new ClassLoader[0]));
             CompletableFuture.supplyAsync(() -> {
                 Thread.currentThread().setContextClassLoader(classLoader);
                 DefaultResourceLoader loader = new DefaultResourceLoader(classLoader);
-                SpringApplication springApplication = new ForgeSpringApplication(loader, mods);
-                return springApplication.run();
-            }, executorService).get();
+                AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+                ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+                context.setResourceLoader(loader);
+                context.scan(mods.stream().map(mod -> {
+                    beanFactory.registerResolvableDependency(mod.getClass(), mod);
+                    String componentPackage = mod.getClass().getPackage().getName();
+                    log.info("Spring scans all components located in {} and beyond", componentPackage);
+                    return componentPackage;
+                }).toArray(String[]::new));
+                return context;
+            }, executorService).get().refresh();
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         } finally {
--- src/main/java/ru/ckateptb/itlfts/SpringSource.java
+++
@@ -1,4 +0,0 @@
-package ru.ckateptb.itlfts;
-
-public class SpringSource {
-}
