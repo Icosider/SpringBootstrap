3c7689524729d38d59f8ea613caf31fe5fe749e7
CKATEPTb <taksis@ex.ua>
2020-12-24 06:23:42 +0200

    Apache lang to lang3

--- src/main/java/ru/ckateptb/itlfts/schedule/api/AbstractScheduledExecutorService.java
+++ src/main/java/ru/ckateptb/itlfts/schedule/api/AbstractScheduledExecutorService.java
@@ -1,6 +1,6 @@
 package ru.ckateptb.itlfts.schedule.api;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.Collection;
 import java.util.List;
@@ -9,81 +9,81 @@ import java.util.concurrent.*;
 public abstract class AbstractScheduledExecutorService implements ScheduledExecutorService {
     @Override
     public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public void shutdown() {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public List<Runnable> shutdownNow() {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public boolean isShutdown() {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public boolean isTerminated() {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public boolean awaitTermination(long timeout, TimeUnit unit) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public <T> Future<T> submit(Callable<T> task) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public <T> Future<T> submit(Runnable task, T result) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public Future<?> submit(Runnable task) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public <T> T invokeAny(Collection<? extends Callable<T>> tasks) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 
     @Override
     public void execute(Runnable command) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Code is not implement!");
     }
 }
