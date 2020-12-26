49edd635baea33ad37fcdcd5d5cfffa37aa6479b
CKATEPTb <taksis@ex.ua>
2020-12-26 10:15:45 +0200

    Add method description

--- src/main/java/ru/ckateptb/itlfts/temporary/api/Temporary.java
+++ src/main/java/ru/ckateptb/itlfts/temporary/api/Temporary.java
@@ -1,9 +1,27 @@
 package ru.ckateptb.itlfts.temporary.api;
 
+/**
+ * @see AbstractTemporary
+ */
 public interface Temporary {
+    /**
+     * @see AbstractTemporary#process()
+     */
     void process();
+
     void onProcess();
+
+    /**
+     * Возвращает исходное положение текущего объекта
+     * @see AbstractTemporary#revert()
+     */
     void revert();
+
     void onRevert();
+
+    /**
+     * @return Время в мс, когда текущией объект должен вернуться в исходное положение
+     * @see AbstractTemporary#getExpireTime()
+     */
     long getExpireTime();
 }
