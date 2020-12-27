56f985aea8a763263d17966f3c1013b9e825a22e
CKATEPTb <taksis@ex.ua>
2020-12-27 11:11:28 +0200

    Remove async context initializer

--- src/main/java/ru/ckateptb/itlfts/spring/MinecraftForgeSpringContextInitializer.java
+++ src/main/java/ru/ckateptb/itlfts/spring/MinecraftForgeSpringContextInitializer.java
@@ -4,14 +4,10 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import net.minecraftforge.fml.ModWorkManager;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.http.util.Asserts;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.ckateptb.itlfts.utils.FutureUtils;
-
-import java.util.concurrent.CompletableFuture;
 
 @Log4j2
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -26,17 +22,16 @@ public class MinecraftForgeSpringContextInitializer {
     @SneakyThrows
     public static AnnotationConfigApplicationContext initializeContext(Object mod) {
         Class<?> clazz = mod.getClass();
-        Asserts.check(mod.getClass().isAnnotationPresent(Mod.class), "{} is not mod main class", clazz);
-        return FutureUtils.consume(CompletableFuture.supplyAsync(() -> {
-            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-            ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
-            // TODO Каждый мод работает отдельно в своем modloading-worker, их бы объединить все.
-            log.info("Found a mod that requires Spring: {}", clazz);
-            beanFactory.registerResolvableDependency(clazz, mod);
-            String packageName = clazz.getPackage().getName();
-            log.info("Spring scans all components located in {} and beyond", packageName);
-            context.scan(packageName);
-            return context;
-        }, ModWorkManager.syncExecutor()).get(), AnnotationConfigApplicationContext::refresh);
+        Asserts.check(clazz.isAnnotationPresent(Mod.class), "{} is not mod main class", clazz);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+        // TODO Каждый мод работает отдельно в своем modloading-worker, их бы объединить все.
+        log.info("Found a mod that requires Spring: {}", clazz);
+        beanFactory.registerResolvableDependency(clazz, mod);
+        String packageName = clazz.getPackage().getName();
+        log.info("Spring scans all components located in {} and beyond", packageName);
+        context.scan(packageName);
+        context.refresh();
+        return context;
     }
 }
