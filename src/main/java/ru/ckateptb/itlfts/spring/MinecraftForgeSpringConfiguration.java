4f6457505954d12b01baf4f378a5e301f655a134
CKATEPTb <taksis@ex.ua>
2020-12-27 11:10:58 +0200

    Add logs for loaded beans

--- src/main/java/ru/ckateptb/itlfts/spring/MinecraftForgeSpringConfiguration.java
+++ src/main/java/ru/ckateptb/itlfts/spring/MinecraftForgeSpringConfiguration.java
@@ -1,5 +1,6 @@
 package ru.ckateptb.itlfts.spring;
 
+import lombok.extern.log4j.Log4j2;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,7 +14,9 @@ import ru.ckateptb.itlfts.listeter.Listener;
 import ru.ckateptb.itlfts.schedule.ScheduleService;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
+@Log4j2
 @Configuration
 public class MinecraftForgeSpringConfiguration implements SchedulingConfigurer {
     private final AnnotationConfigApplicationContext context;
@@ -25,6 +28,8 @@ public class MinecraftForgeSpringConfiguration implements SchedulingConfigurer {
     @EventListener
     public void onStartup(ContextRefreshedEvent event) {
         Collection<Listener> beans = context.getBeansOfType(Listener.class).values();
+        log.info("Scan completed successfully, bean list: \n\t{}", context.getBeansOfType(Object.class).values()
+                .stream().map(Object::getClass).map(Class::toString).collect(Collectors.joining("\n\t")));
         IEventBus eventBus = context.getBean(IEventBus.class);
         beans.forEach(eventBus::register);
     }
