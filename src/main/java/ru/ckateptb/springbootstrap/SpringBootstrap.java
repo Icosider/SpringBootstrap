482d1745d4f3a53b2e617933e48ed841cf995975
CKATEPTb <taksis@ex.ua>
2020-12-29 10:30:43 +0200

    Register server bean

--- src/main/java/ru/ckateptb/springbootstrap/SpringBootstrap.java
+++ src/main/java/ru/ckateptb/springbootstrap/SpringBootstrap.java
@@ -1,7 +1,10 @@
 package ru.ckateptb.springbootstrap;
 
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import ru.ckateptb.springbootstrap.spring.MinecraftForgeSpringContextInitializer;
 import ru.ckateptb.springbootstrap.spring.mod.SpringMod;
@@ -9,10 +12,17 @@ import ru.ckateptb.springbootstrap.spring.mod.SpringMod;
 @Mod("springbootstrap")
 public class SpringBootstrap extends SpringMod {
     public SpringBootstrap() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::on);
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::onCommonSetupEvent);
+        bus.addListener(this::onServerStartingEvent);
     }
 
-    public void on(FMLCommonSetupEvent event) {
+    public void onCommonSetupEvent(FMLCommonSetupEvent event) {
         MinecraftForgeSpringContextInitializer.initialize();
+    }
+
+    public void onServerStartingEvent(FMLServerStartingEvent event) {
+        MinecraftServer server = event.getServer();
+        getContext().getBeanFactory().registerResolvableDependency(server.getClass(), server);
     }
 }
